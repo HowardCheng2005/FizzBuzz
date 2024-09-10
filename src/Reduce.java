06f963fbe37aa812e2c256d41a941babb136c85e
HowardCheng <howardhx.cheng@mail.utoronto.ca>
2024-09-10 14:15:20 -0400

    added reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,5 @@
 public class Reduce {
-    public static void main(String[] args) {
+    public static int main(String[] args) {
 
         int i = 100;
         int stepsToZero = 0;
@@ -13,5 +13,6 @@ public class Reduce {
             }
         }
         System.out.println("there are " + stepsToZero + " steps to zero");
+        return stepsToZero;
     }
 }
